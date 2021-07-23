77e8b140436bc81a2a2b5b4be9eed0655716657d
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-23 17:15:52 -0500

    ShorthandProperties

--- src/index.js
+++ src/index.js
@@ -477,3 +477,60 @@ function ObjectandArrayDestructuring(){
 // }
 // Now we still have profile and repos, but those are being created with array destructuring inside of the function’s parameters.
 }
+
+
+function ShorthandProperties(){
+    
+// With Shorthand Properties, whenever you have a variable which is the same name as a property on an object, when constructing the object, you can omit the property name.
+
+// What that means is that code that used to look like this,
+
+// function formatMessage (name, id, avatar) {
+//   return {
+//     name: name,
+//     id: id,
+//     avatar: avatar,
+//     timestamp: Date.now()
+//   }
+// }
+// can now look like this.
+
+// function formatMessage (name, id, avatar) {
+//   return {
+//     name,
+//     id,
+//     avatar,
+//     timestamp: Date.now()
+//   }
+// }
+// Shorthand Method Names
+// Now, what if one of those properties was a function?
+
+// A function that is a property on an object is called a method. With ES6’s Shorthand Method Names, you can omit the function keyword completely. What that means is that code that used to look like this,
+
+// function formatMessage (name, id, avatar) {
+//   return {
+//     name,
+//     id,
+//     avatar,
+//     timestamp: Date.now(),
+//     save: function () {
+//       // save message
+//     }
+//   }
+// }
+// can now look like this
+
+// function formatMessage (name, id, avatar) {
+//   return {
+//     name,
+//     id,
+//     avatar,
+//     timestamp: Date.now(),
+//     save () {
+//       //save message
+//     }
+//   }
+// }
+}
+
